1eca5c9efc9a291236bea24db451843eb2c25d89
p.bykov <p.bykov@qiwi.com>
2024-11-20 17:36:57 +0300

    add record in file

--- revers.py
+++ revers.py
@@ -6,4 +6,7 @@ count = sys.argv[1]
 for i in range(int(count)):
     ip=input("Введите ip:\n").split('.')
     ip.reverse()
-    print("PTR: " + '.'.join(ip) + ".in-addr.arpa.")
+    record = '.'.join(ip) + ".in-addr.arpa."
+    file = open(sys.argv[3], 'a')
+    file.write(record.ljust(54) + "3600   IN  PTR    " + sys.argv[2] + "." + '\n')
+    file.close()
