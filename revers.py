aca1ec953ca43fc2963a7ff0dcaaed6f03b83edc
p.bykov <p.bykov@qiwi.com>
2024-11-21 10:46:19 +0300

    add func tail

--- revers.py
+++ revers.py
@@ -1,12 +1,30 @@
 #!python3.12
-import sys
+from sys import argv
 
 count = sys.argv[1]
 
-for i in range(int(count)):
-    ip=input("Введите ip:\n").split('.')
-    ip.reverse()
-    record = '.'.join(ip) + ".in-addr.arpa."
-    file = open(sys.argv[4], 'a')
-    file.write(record.ljust(54) + f"{sys.argv[2]}   IN  PTR    " + sys.argv[3] + "." + '\n')
-    file.close()
+
+def tail(fnam: str, k: int):
+    with open(fnam,"r") as fi:
+        txt=fi.readlines()
+        count=len(txt)
+        for i in range(count-k,count):
+            print(txt[i],end='')
+
+
+def main():
+    for i in range(int(count)):
+        try:
+            ip=input("Введите ip:\n").split('.')
+            ip.reverse()
+            record = '.'.join(ip) + ".in-addr.arpa."
+            file = open(sys.argv[4], 'a')
+            file.write(record.ljust(54) + f"{sys.argv[2]}   IN  PTR    " + sys.argv[3] + "." + '\n')
+            file.close()
+            tail(sys.argv[4], 4)
+        except Exeption as e:
+            print(e)
+
+
+if __name__ == '__main__':
+    main()
